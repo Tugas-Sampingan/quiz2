46c89d3ecb3edab2127643ce066a8db12f37c8b6
dhanakbar <maulana261100@gmail.com>
2021-12-21 23:28:29 +0800

    update login dan navbar

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -8,26 +9,29 @@
 </head>
 
 <body>
-    <script src= "js/bootsrap.js"></script>
-    <script src="js/popper.min.js"></script>
-    <br><br><br>
-    <h1 style="text-align: center;"><b>LOGIN TO OZARK GLASSES STORE</b></h1>
-    <form>
-      <div class="container">
-        <div class="row mb-3">
-          <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-          <div class="col-sm-10">
-            <input type="email" class="form-control" id="inputEmail3">
-          </div>
-        </div>
-        <div class="row mb-3">
-          <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-          <div class="col-sm-10">
-            <input type="password" class="form-control" id="inputPassword3">
-          </div>
-        </div>
-        <a class="btn btn-primary mx-auto" href="home.html">LOG IN USER</a>
-        <a class="btn btn-primary mx-auto" href="admin.html">LOG IN ADMIN</a>
-      </div>
-</body>
+  <script src="js/bootsrap.js"></script>
+  <script src="js/popper.min.js"></script>
+  <br><br><br>
+  <center>
 
+    <form>
+      <table>
+        <h1 style="text-align: center;"><b>LOGIN TO OZARK GLASSES STORE</b></h1>
+        <tr>
+          <td><label for="inputEmail3" class="col-sm-2 col-form-label">Email</label></td>
+          <td><input type="email" class="form-control" id="inputEmail3"></td>
+        </tr>
+        <tr>
+          <td><label for="inputPassword3" class="col-sm-2 col-form-label">Password</label></td>
+          <td> <input type="password" class="form-control" id="inputPassword3"></td>
+        </tr>
+
+        <tr>
+          <td><br><a class="btn btn-primary mx-auto" href="home.php">LOG IN USER</a></td>
+          <td><br><a class="btn btn-primary mx-auto" href="admin.php">LOG IN ADMIN</a></td>
+        </tr>
+
+      </table>
+
+  </center>
+</body>
--- navbar.php
+++ navbar.php
@@ -21,7 +21,7 @@
                     </li>
                 </ul>
                 <form class="d-flex">
-                    <a class="btn btn-primary border-0" style="background-color:#696969 ; margin-right: 8px;">Sign In</a>
+                    <a class="btn btn-primary border-0" style="background-color:#696969 ; margin-right: 8px;" href="login.php">Sign In</a>
                     <a class="btn btn-primary border-0" style="background-color: #696969 ;">Sign Up</a>
                 </form>
             </div>
