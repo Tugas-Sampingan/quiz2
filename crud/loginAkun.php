aaa5173ce0ec4b24300d166833073d00cd2590f1
dhanakbar <maulana261100@gmail.com>
2021-12-22 14:16:14 +0800

    update login

--- crud/loginAkun.php
+++ crud/loginAkun.php
@@ -12,15 +12,31 @@ $numRow = mysqli_num_rows($tabel);
 
 $checkEmail;
 
-foreach ($tabel as $row) :
-    if ($row['email'] == $email) {
-        $checkEmail = $row["email"];
-        if ($row['password'] == $password) {
-            header("location: http://localhost/quiz2");
+//ntar kasih section supaya tau mana admin mana user
+if (isset($_POST['login-user'])) {
+    foreach ($tabel as $row) :
+        if ($row['email'] == $email) {
+            $checkEmail = $row["email"];
+            if ($row['password'] == $password) {
+                header("location: http://localhost/quiz2");
+                break;
+            } else {
+                header("location: http://localhost/quiz2/login.php");
+            }
             break;
-        } else {
-            header("location: http://localhost/quiz2/login.php");
         }
-        break;
-    }
-endforeach;
+    endforeach;
+} else if (isset($_POST['login-admin'])) {
+    foreach ($tabel as $row) :
+        if ($row['email'] == $email) {
+            $checkEmail = $row["email"];
+            if ($row['password'] == $password) {
+                header("location: http://localhost/quiz2");
+                break;
+            } else {
+                header("location: http://localhost/quiz2/login.php");
+            }
+            break;
+        }
+    endforeach;
+}
