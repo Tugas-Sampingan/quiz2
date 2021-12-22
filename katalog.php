e622df2c4d15c8b0ad254962105486b8f9595dee
Gilang Gumelar <gilanggumelar1945@gmail.com>
2021-12-22 14:16:47 +0700

    katalog

--- katalog.php
+++ katalog.php
@@ -1,4 +1,10 @@
-<!DOCTYPE html>
+<?php
+
+include 'koneksi.php';
+
+$selected = mysqli_query($conn, "SELECT * FROM databarang");
+
+?>
 <html lang="en">
 
 <head>
@@ -16,107 +22,37 @@
 
   <section id="katalog">
     <h3 style="text-align: center;">OZARK'S CATALOG</h3>
-    <div class="row row-cols-1 row-cols-md-2 g-4">
-      <div class="col">
-        <div class="card">
-          <img src="https://www.optiktunggal.com/img/product/rb4246v-1.JPG" class="card-img-top" alt="p1" height="500px" width="100px">
-          <div class="card-body">
-            <h4 class="card-title" style="text-align: center;">CHARIZ BLACK</h4>
-            <p class="card-text">Kode: CHARIZ, Harga: Rp109.000.
-              Si bulat kokoh dan stylish bikin penampilanmu jadi makin kecee😎 Unisex cewe cowo bisa pakai👌.
-              Ringan slim tipis nyaman dipakai so comfy! Recommended😍
-            <ul type="disc">
-              <h6>Estimasi Detail Size:</h6>
-              <li>Lebar lensa: 4,9 cm</li>
-              <li>Lebar nosepad (jarak antara lensa kanan kiri) : 2,0 cm</li>
-              <li>Panjang gagang : 14,5 cm</li>
-              <li>Lebar seluruh lensa (ujung lensa kanan ke kiri): 11,8 cm</li>
-              <li>Tinggi frame: 4,7 cm</li>
-              <li>material: plastik mix besi, frame lentur (+) per (-) Penyangga hidung tidak bisa diatur</li>
-            </ul>
-            </p>
+
+    <div class="container">
+      <div class="row row-cols-1 row-cols-md-2 g-4">
+        <?php foreach ($selected as $row) : ?>
+          <div class="col">
+            <div class="card">
+              <img src="katalog/<?= $row["gambar"]; ?>" class="card-img-top" alt="p1" height="360px" width="530px">
+              <div class="card-body">
+                <h4 class="card-title" style="text-align: center;"> <?= $row["nama_barang"]; ?></h4>
+                <p class="card-text">Kode: <?= $row["kode_barang"]; ?> Harga: Rp <?= $row["harga"]; ?>,- <br>
+                  <!-- Deskripsi -->
+                  
+                  <?php $arr = explode ("; ",$row['detail']); ?>
+                  <ul type="disc">
+                    <h6>Detail Size: </h6>
+                    <li>Lebar lensa: <?= $arr[0]; ?></li>
+                    <li>Lebar nosepad (jarak antara lensa kanan kiri) : <?= $arr[1]; ?></li>
+                    <li>Panjang gagang : <?= $arr[2]; ?></li>
+                    <li>Lebar seluruh lensa (ujung lensa kanan ke kiri): <?= $arr[3]; ?></li>
+                    <li>Tinggi frame: <?= $arr[4]; ?></li>
+                    <li>material: <?= $arr[5]; ?></li>
+                  </ul>
+                </p>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img src="https://www.optiktunggal.com/img/product/IMG_0262_copy1.jpg" class="card-img-top" alt="p2" height="500px" width="100px">
-          <div class="card-body">
-            <h5 class="card-title" style="text-align: center;">KALA WHITE LEOPARD</h5>
-            <p class="card-text">Kode: KALA. Harga: Rp100.000. Modelnya kotak simple yang kokoh dan pastinya bikin penampilan kamu makin kece🤓.
-            <ul type="disc">
-              <h6>Estimasi Detail Size</h6>
-              <li>Lebar lensa: 5,1 cm</li>
-              <li>Lebar nosepad (jarak antara lensa kanan kiri) : 2,0 cm</li>
-              <li>Panjang gagang : 13,5 cm</li>
-              <li>Lebar seluruh lensa (ujung lensa kanan ke kiri): 12,2 cm</li>
-              <li>Tinggi frame: 4,5 cm</li>
-            </ul>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img src="https://www.optiktunggal.com/img/product/IMG_0146_copy.jpg" class="card-img-top" alt="p3" height="500px" width="100px">
-          <div class="card-body">
-            <h5 class="card-title" style="text-align: center;">AGNES B</h5>
-            <p class="card-text">Kode: AGNES. Harga: Rp110.000. Sophisticated stylish keceeee bangett! slim dan kokoh nyaman banget dipakai seharian 🤓❤️
-              RECOMMENDED MUST HAVE✨
-            <ul type="disc">
-              <h6>Estimasi Detail Size</h6>
-              <li>Lebar lensa: 5,3 cm</li>
-              <li>Lebar nosepad (jarak antara lensa kanan kiri) : 1,5 cm</li>
-              <li>Panjang gagang : 13,8 cm</li>
-              <li>Lebar seluruh lensa (ujung lensa kanan ke kiri): 12,1 cm</li>
-              <li>Tinggi frame: 4,8cm</li>
-              <li>material: plastik mix besi,Penyangga hidung bisa diatur</li>
-            </ul>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img src="https://www.optiktunggal.com/img/product/IMG_0254_copy1.jpg" class="card-img-top" alt="p4" height="500px" width="100px">
-          <div class="card-body">
-            <h5 class="card-title" style="text-align: center;">RONA BLACK</h5>
-            <p class="card-text">Kode: RB. Harga: Rp100.000. Bentuk semi Cat eyes mungil ini bikin penampilan kamu semakin elegant dan kece bgt😍. Bisa Request lensa minus/plus/silinder
-            <ul type="disc">
-              <h6>Estimasi Detail Size </h6>
-              <li>Lebar lensa: 5,4cm</li>
-              <li>Lebar nosepad (jarak antara lensa kanan kiri) : 2cm</li>
-              <li>Panjang gagang : 14,2cm</li>
-              <li>Lebar seluruh lensa (ujung lensa kanan ke kiri): 12,8cm</li>
-              <li>Tinggi frame: 4,4cm</li>
-              <li>aterial: plastik mix besi,Penyangga hidung bisa diatur</li>
-            </ul>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img src="https://www.optiktunggal.com/img/product/0RB4278__628211_000A.jpg" class="card-img-top" alt="p5" height="500px" width="100px">
-          <div class="card-body">
-            <h5 class="card-title" style="text-align: center;">ELENZE IN SQUARE</h5>
-            <p class="card-text">Super stylish😎 cocok untuk nemenin liburan kamu jadi makin hype!
-              Unisex cewe cowo bisa pakai 😍. Kacamata sudah dilengkapi lensa anti UV!
-            <ul type="disc">
-              <h6>Estimasi Detail Size</h6>
-              <li>Lebar lensa: 6,4 cm</li>
-              <li>Lebar nosepad (jarak antara lensa kanan kiri) : 1,3 cm</li>
-              <li>Panjang gagang : 15,8 cm</li>
-              <li>Lebar seluruh lensa (ujung lensa kanan ke kiri): 14,1 cm</li>
-              <li>Tinggi frame: 5,4 cm</li>
-              <li>material: plastik mix besi,Penyangga hidung tidak bisa diatur</li>
-            </ul>
-            </p>
-          </div>
-        </div>
+        <?php endforeach; ?>    
       </div>
     </div>
   </section>
+
   <!-- footer -->
   <footer class="mt-4">
     <div class="container">
